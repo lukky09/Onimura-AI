ba6abd62bbff89418914b8b604ed1babd908e787
lukky09 <64144285+lukky09@users.noreply.github.com>
2021-11-23 22:42:26 +0700

    Mbuat Papan

---
+++ Onimura_AI/Assets/Script/Board.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour 
+{
+
+    GameObject[,] objarr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objarr = new GameObject[5,5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
